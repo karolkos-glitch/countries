0af4d6755d041078d6680e1b3c153e1279bed504
karolkos-glitch <karol99kos@gmail.com>
2020-03-29 18:52:48 +0200

    redux-thunk installed

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import reducers from './reducers';
 import { createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { applyMiddleware } from 'react-redux';
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 export default store;
