4229b16d51501800ae6ca30d6ab12374e3c18b2d
karolkos-glitch <karol99kos@gmail.com>
2020-03-30 20:53:26 +0200

    Updates

---
+++ precache-manifest.850fb8d6ddf34fa5b3474efe0e587d3e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c94ed050d104395d09a76b3b7abc35a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1d5924b0feeb1d6dc0e",
+    "url": "/static/js/2.887cb0a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.887cb0a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f24e42a5c9bd077625f",
+    "url": "/static/js/3.27fae273.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.27fae273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80bbfde8263c0612ad4c",
+    "url": "/static/js/4.18fd6aac.chunk.js"
+  },
+  {
+    "revision": "148d66c047f1ffd4dacb",
+    "url": "/static/js/5.4e19c604.chunk.js"
+  },
+  {
+    "revision": "038e4a52bec91b90194d",
+    "url": "/static/js/main.cfdab1b0.chunk.js"
+  },
+  {
+    "revision": "c5dc40b50d336e6f5742",
+    "url": "/static/js/runtime-main.7a7d1723.js"
+  }
+]);
